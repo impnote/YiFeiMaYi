cc6bf68fd6601ca76c0c662bbd260148ca3000b0
施顾杰 <immpnote@gmail.com>
2016-12-15 01:12:40 +0800

    update thymeleaf templates

--- src/main/java/com/yifei/HelloWorldController.java
+++ src/main/java/com/yifei/HelloWorldController.java
@@ -1,19 +1,19 @@
 package com.yifei;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by apple on 2016/12/9.
  */
-@RestController
+@Controller
 @RequestMapping(path = "/test")
 public class HelloWorldController {
 
-    @RequestMapping(path = "/demo",method = RequestMethod.GET)
+    @RequestMapping(path = "/demo")
     public String sayHello() {
-         String a = "hello";
-        return a;
+        return "index";
     }
+
+
 }
--- src/main/java/com/yifei/IndexController.java
+++ src/main/java/com/yifei/IndexController.java
@@ -1,13 +1,20 @@
 package com.yifei;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by apple on 2016/12/13.
  */
-@RequestMapping("/")
+@Controller
+@RequestMapping("/hello")
 public class IndexController {
+    @RequestMapping("")
     public String index() {
-        return "index";
+        return "hello";
+    }
+    @RequestMapping("/contact")
+    public String contact() {
+        return "contact-us";
     }
 }
